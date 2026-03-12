68f3a909c95a2d28d571199a0a30742cfdda699f
Itallo Sousa <itallosousa@MacBook-Air-de-Itallo-2.local>
2026-03-12 18:13:14 -0300

    style: remove benefits section from landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Hero from './components/Hero.jsx';
-import Benefits from './components/Benefits.jsx';
 import Products from './components/Products.jsx';
 import Footer from './components/Footer.jsx';
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="min-h-screen">
       <Hero />
-      <Benefits />
       <Products />
       <Footer />
     </div>
